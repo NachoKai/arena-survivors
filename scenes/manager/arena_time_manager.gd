f603e6f1c834f5540023d9c6e9e6002a182c79eb
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-09-30 23:11:31 -0300

    Add timer on the ui

---
+++ scenes/manager/arena_time_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+@onready var timer = $Timer
+
+
+func get_time_elapsed():
+	return timer.wait_time - timer.time_left
---
+++ scenes/ui/arena_time_ui.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+@export var arena_time_manager: Node
+@onready var label = %Label
+
+
+func _process(delta):
+	if arena_time_manager == null: return
+	var time_elapsed = arena_time_manager.get_time_elapsed()
+	label.text = format_seconds_to_string(time_elapsed)
+	
+
+func format_seconds_to_string(seconds: float) -> String:
+	var minutes = floor(seconds / 60)
+	var remaining_seconds = floor(seconds - (minutes * 60))
+	return ("%02d" % minutes) + ":" + ("%02d" % remaining_seconds) # %02d adds a leading zero, so 0:0 becomes 00:00
