85ef6e09e8d977bb4c0dc7fbca5637ec81735ed9
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-10-01 17:05:10 -0300

    Add experience bar

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -1,6 +1,11 @@
 extends Node
 
+signal experience_updated(current_experience: float, target_experience: float)
+
+const TARGET_EXPERIENCE_GROWTH: int = 5
 var current_experience: float = 0
+var current_level: float = 1
+var target_experience: float = 5
 
 
 func _ready():
@@ -8,9 +13,13 @@ func _ready():
 
 
 func increment_experience(number: float):
-	current_experience += number
-	print(current_experience)
-
+	current_experience = min(current_experience + number, target_experience)
+	experience_updated.emit(current_experience, target_experience)
+	if current_experience == target_experience:
+		current_level += 1
+		target_experience += TARGET_EXPERIENCE_GROWTH
+		current_experience = 0
+		experience_updated.emit(current_experience, target_experience)
 
 func on_experience_vial_collected(number: float):
 	increment_experience(number)
