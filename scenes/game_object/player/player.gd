a4b925daf4dcc3a130ff594d646f469a6009e8f3
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-10-02 23:03:26 -0300

    Add player health bar

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,15 +1,21 @@
 extends CharacterBody2D
 
 @onready var damage_interval_timer: Timer = $DamageIntervalTimer
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var health_bar: ProgressBar = $HealthBar
+@onready var collision_area: Area2D = $CollisionArea
+
 const MAX_SPEED: int = 125
 const ACCELERATION_SMOOTHING: int = 25
 var colliding_bodies_quantity: int = 0
 
 
 func _ready():
-	$CollisionArea.body_entered.connect(on_body_entered)
-	$CollisionArea.body_exited.connect(on_body_exited)
+	collision_area.body_entered.connect(on_body_entered)
+	collision_area.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+	health_component.health_changed.connect(on_health_changed)
+	update_health_display()
 
 
 func _process(delta):
@@ -21,18 +27,26 @@ func _process(delta):
 
 func check_deal_damage():
 	if colliding_bodies_quantity == 0 || damage_interval_timer.is_stopped(): return
-	$HealthComponent.damage(1)
+	health_component.damage(1)
 	damage_interval_timer.start()
 
 
-func on_body_entered(other_body: Node2D):
+func update_health_display():
+	health_bar.value = health_component.get_health_percent()
+
+
+func on_body_entered(_other_body: Node2D):
 	colliding_bodies_quantity += 1
 	check_deal_damage()
 
 
-func on_body_exited(other_body: Node2D):
+func on_body_exited(_other_body: Node2D):
 	colliding_bodies_quantity -= 1
 
 
 func on_damage_interval_timer_timeout():
 	check_deal_damage()
+
+
+func on_health_changed():
+	update_health_display()
