e1ad83f4738a3f14eb5bb2e42ef770c916b8d85a
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-10-03 21:59:55 -0300

    Increase difficulty over time

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,11 +1,23 @@
 extends Node
 
+signal arena_difficulty_increased(arena_difficulty: int)
+
 @export var end_screen_scene: PackedScene
-@onready var timer = $Timer
+@onready var timer: Timer = $Timer
+
+const DIFFICULTY_INTERVAL: int = 5
+var arena_difficulty: int = 0
 
 
 func _ready():
 	timer.timeout.connect(on_timer_timeout)
+
+
+func _process(_delta):
+	var next_time_target = timer.wait_time - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
+	if timer.time_left <= next_time_target:
+		arena_difficulty += 1
+		arena_difficulty_increased.emit(arena_difficulty)
 
 
 func get_time_elapsed():
--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -1,15 +1,21 @@
 extends Node
 
 @export var basic_enemy_scene: PackedScene
-const SPAWN_RADIUS = 375
+@export var arena_time_manager: Node
+@onready var timer: Timer = $Timer
+
+const SPAWN_RADIUS: int = 375
+var base_spawn_time: float = 0
 
 
 func _ready():
-	$Timer.timeout.connect(on_timer_timeout)
+	base_spawn_time = timer.wait_time
+	timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
 
 func on_timer_timeout():
-	# spawn enemies on screen
+	timer.start()
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null: return
 	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))  # TAU: 2 times PI, a full rotation
@@ -18,3 +24,9 @@ func on_timer_timeout():
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position
+
+
+func on_arena_difficulty_increased(arena_difficulty: int):
+	var time_off = min((0.1 / 60 / 5) * arena_difficulty, 0.7)
+	timer.wait_time = base_spawn_time - time_off
+
