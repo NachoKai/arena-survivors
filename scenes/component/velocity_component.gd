6101c77397e1efd7a90524f02af05359b6d4c38f
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-10-08 11:29:35 -0300

    Add wizard enemy animation

--- scenes/component/velocity_component.gd
+++ scenes/component/velocity_component.gd
@@ -12,12 +12,16 @@ func accelerate_to_player():
 	if player == null: return
 	var direction = (player.global_position - owner_node2d.global_position).normalized()
 	accelerate_in_direction(direction)
-	
+
 
 func accelerate_in_direction(direction: Vector2):
 	var desired_velocity = direction * max_speed
 	velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * get_process_delta_time()))
-	
+
+
+func decelerate():
+	accelerate_in_direction(Vector2.ZERO)
+
 
 func move(character_body: CharacterBody2D):
 	character_body.velocity = velocity
--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -3,10 +3,16 @@ extends CharacterBody2D
 @onready var visuals: Node2D = $Visuals
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var velocity_component: Node = $VelocityComponent
+var is_moving: bool = false
 
 
 func _process(_delta):
-	velocity_component.accelerate_to_player()
+	if is_moving: velocity_component.accelerate_to_player()
+	else: velocity_component.decelerate()
 	velocity_component.move(self)
 	var move_sign = sign(velocity.x)
 	if move_sign != 0: visuals.scale = Vector2(move_sign, 1)
+
+
+func set_is_moving(moving: bool):
+	is_moving == moving
