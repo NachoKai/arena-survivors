6856ea3f2a77d174d57a6deb0253b124225f0f17
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-10-08 15:26:52 -0300

    Add floating number damage

--- scenes/component/hurtbox_component.gd
+++ scenes/component/hurtbox_component.gd
@@ -2,6 +2,7 @@ extends Area2D
 class_name HurtboxComponent
 
 @export var health_component: HealthComponent
+var floating_text_scene = preload("res://scenes/ui/floating_text.tscn")
 
 
 func _ready():
@@ -12,4 +13,10 @@ func on_area_entered(other_area: Area2D):
 	if not other_area is HitboxComponent: return
 	if health_component == null: return
 	var hitbox_component = other_area as HitboxComponent
-	health_component.damage(hitbox_component.damage)
+	var damage = hitbox_component.damage
+	health_component.damage(damage)
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	if floating_text == null: return
+	get_tree().get_first_node_in_group("foreground").add_child(floating_text)
+	floating_text.global_position = global_position + (Vector2.UP * 8)
+	floating_text.start(str(damage))
---
+++ scenes/ui/floating_text.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var label: Label = $Label
+
+
+func _ready():
+	pass
+
+
+func start(text: String):
+	label.text = text
+	var tween = create_tween()
+	const DURATION = 0.3
+	tween.set_parallel()
+	tween.tween_property(self, "global_position", global_position + (Vector2.UP * 16), DURATION).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2.ONE * 1.2, DURATION).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+	tween.chain()
+	tween.tween_property(self, "scale", Vector2.ONE * 0.8, DURATION).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+	tween.chain()
+	tween.tween_property(self, "global_position", global_position + (Vector2.DOWN * 48), DURATION).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2.ZERO, DURATION).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	tween.chain()
+	tween.tween_callback(queue_free)
