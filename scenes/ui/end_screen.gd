1c383a597e063ca774e33d47f46a4e2d26eff2f1
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-10-10 19:10:27 -0300

    Animate end screen

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -1,15 +1,25 @@
 extends CanvasLayer
 
+@onready var panel_container: PanelContainer = %PanelContainer
+@onready var restart_button: Button = %RestartButton
+@onready var quit_button: Button = %QuitButton
+@onready var title_label: Label = %TitleLabel
+@onready var description_label: Label = %DescriptionLabel
+
 
 func _ready():
+	panel_container.pivot_offset = panel_container.size / 2	
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0.3).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 	get_tree().paused = true
-	%RestartButton.pressed.connect(on_restart_button_pressed)
-	%QuitButton.pressed.connect(on_quit_button_pressed)
+	restart_button.pressed.connect(on_restart_button_pressed)
+	quit_button.pressed.connect(on_quit_button_pressed)
 
 
 func set_defeat():
-	%TitleLabel.text = "Defeat"
-	%DescriptionLabel.text = "You lost!"
+	title_label.text = "Defeat"
+	description_label.text = "You lost!"
 	
 
 func on_restart_button_pressed():
