16b33d8afe6f5077a1f4fbd0c482a164509181e2
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-10-11 21:06:45 -0300

    Add ui button sounds

---
+++ scenes/component/random_audio_stream_player_component.gd
@@ -0,0 +1,14 @@
+extends AudioStreamPlayer
+
+@export var streams: Array[AudioStream]
+@export var randomize_pitch: bool = true
+@export var min_pitch: float = 0.9
+@export var max_pitch: float = 1.1
+
+
+func play_random():
+	if streams == null || streams.size() == 0: return
+	if randomize_pitch: pitch_scale = randf_range(min_pitch, max_pitch)
+	else: pitch_scale = 1
+	stream = streams.pick_random()
+	play()
---
+++ scenes/ui/sound_button.gd
@@ -0,0 +1,11 @@
+extends Button
+
+@onready var random_audio_stream_player_component = $RandomAudioStreamPlayerComponent
+
+
+func _ready():
+	pressed.connect(on_pressed)
+	
+	
+func on_pressed():
+	random_audio_stream_player_component.play_random()
