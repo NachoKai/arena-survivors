263ea47db7838e02368d6e14c36aa0d55a4df4ac
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-10-11 21:49:43 -0300

    Add music

---
+++ scenes/autoload/music_player.gd
@@ -0,0 +1,16 @@
+extends AudioStreamPlayer
+
+@onready var timer: Timer = $Timer
+
+
+func _ready():
+	finished.connect(on_finished)
+	timer.timeout.connect(on_timer_timeout)
+
+
+func on_finished():
+	timer.start()
+
+
+func on_timer_timeout():
+	play()
