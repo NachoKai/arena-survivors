ac793762f41526593ccc37f1f61f5f5963bcfe7a
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-10-15 14:42:09 -0300

    Fix upgrade screen

--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -14,12 +14,13 @@ func _ready():
 func set_ability_upgrade(upgrades: Array[AbilityUpgrade]):
 	var delay = 0
 	for upgrade in upgrades:
-		var card_instance = upgrade_card_scene.instantiate()
-		card_container.add_child(card_instance)
-		card_instance.set_ability_upgrade(upgrade)
-		card_instance.play_in(delay)
-		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
-		delay += 0.2
+		if card_container.get_child_count() < 4:
+			var card_instance = upgrade_card_scene.instantiate()
+			card_container.add_child(card_instance)
+			card_instance.set_ability_upgrade(upgrade)
+			card_instance.play_in(delay)
+			card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
+			delay += 0.2
 
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
