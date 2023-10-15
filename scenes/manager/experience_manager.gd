4b3397ea515b1cecc1b8f5050c886535f077c0fe
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-10-15 14:25:34 -0300

    Change exp vial color

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -3,7 +3,7 @@ extends Node
 signal experience_updated(current_experience: float, target_experience: float)
 signal level_up(new_level: int)
 
-const TARGET_EXPERIENCE_GROWTH: int = 5
+const EXPERIENCE_GROWTH_FACTOR: int = 5
 var current_experience: float = 0
 var current_level: float = 1
 var target_experience: float = 5
@@ -14,13 +14,13 @@ func _ready():
 
 
 func increment_experience(number: float):
-	current_experience = min(current_experience + number, target_experience)
-	experience_updated.emit(current_experience, target_experience)
+	current_experience += number
+	experience_updated.emit(min(current_experience, target_experience), target_experience)
 
-	if current_experience == target_experience:
+	if current_experience >= target_experience:
 		current_level += 1
-		target_experience += TARGET_EXPERIENCE_GROWTH
-		current_experience = 0
+		current_experience -= target_experience
+		target_experience *= EXPERIENCE_GROWTH_FACTOR
 		experience_updated.emit(current_experience, target_experience)
 		level_up.emit(current_level)
 
