db9d5f653c45c3867406244f3248f7ec8a7a5b4c
Nacho Caiafa <ignacio.caiafa@gmail.com>
2023-10-22 19:32:08 -0300

    Refactor camera 2d

--- scenes/game_object/game_camera/game_camera.gd
+++ scenes/game_object/game_camera/game_camera.gd
@@ -1,22 +1,14 @@
 extends Camera2D
 
-@onready var player = get_tree().get_first_node_in_group("player") as Node2D
-
-var target_position: Vector2 = Vector2.ZERO
+var player: Node2D
 var camera_smoothing: int = 20
 
 
 func _ready():
 	make_current()
+	player = get_tree().get_first_node_in_group("player")
 
 
-func _process(delta):
-	acquire_target()
-	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * camera_smoothing))  #https://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
-
-
-func acquire_target():
-	if not player: return Vector2.ZERO
-	else: target_position = player.global_position
-
-
+func _physics_process(delta):
+	if player:
+		global_position = global_position.lerp(player.global_position, 1.0 - exp(-delta * camera_smoothing)).round()
