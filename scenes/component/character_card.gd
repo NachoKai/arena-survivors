3e312675bee829cc55190289dc280c51af2db0a9
Nacho Caiafa <ignacio.caiafa@gmail.com>
2025-06-19 17:26:45 -0300

    save selected character

--- scenes/component/character_card.gd
+++ scenes/component/character_card.gd
@@ -27,7 +27,37 @@ func handle_use_button():
 	if name_label.text in implemented_characters:
 		use_button.disabled = false
 
+		var selected_character = SaveGame.get_selected_character()
+		if character_id == selected_character:
+			use_button.text = "Selected"
+			use_button.add_theme_color_override("font_color", Color(0.14902, 0.168627, 0.266667, 1))
+			use_button.add_theme_stylebox_override("normal", create_yellow_stylebox())
+			use_button.add_theme_stylebox_override("hover", create_yellow_stylebox())
+			use_button.add_theme_stylebox_override("pressed", create_yellow_stylebox())
+		else:
+			use_button.text = "Use"
+			use_button.remove_theme_color_override("font_color")
+			use_button.remove_theme_stylebox_override("normal")
+			use_button.remove_theme_stylebox_override("hover")
+			use_button.remove_theme_stylebox_override("pressed")
+
+
+func create_yellow_stylebox() -> StyleBoxFlat:
+	var stylebox = StyleBoxFlat.new()
+	stylebox.bg_color = Color(0.996078, 0.905882, 0.380392, 1)
+	stylebox.border_width_left = 2
+	stylebox.border_width_top = 2
+	stylebox.border_width_right = 2
+	stylebox.border_width_bottom = 2
+	stylebox.border_color = Color(0.14902, 0.168627, 0.266667, 1)
+	stylebox.corner_radius_top_left = 4
+	stylebox.corner_radius_top_right = 4
+	stylebox.corner_radius_bottom_right = 4
+	stylebox.corner_radius_bottom_left = 4
+	return stylebox
+
 
 func on_use_button_pressed():
 	if not character_id: return
 	GameEvents.emit_character_selected(character_id)
+	handle_use_button()
--- scenes/ui/characters_menu.gd
+++ scenes/ui/characters_menu.gd
@@ -8,10 +8,23 @@ var character_card_scene = preload("res://scenes/component/character_card.tscn")
 
 func _ready():
 	back_button.pressed.connect(on_back_button_pressed)
+	GameEvents.character_selected.connect(on_character_selected)
 	for character in characters:
 		var character_card_instance = character_card_scene.instantiate()
 		grid_container.add_child(character_card_instance)
 		character_card_instance.set_character(character)
+
+	refresh_character_cards()
+
+
+func refresh_character_cards():
+	for child in grid_container.get_children():
+		if child.has_method("handle_use_button"):
+			child.handle_use_button()
+
+
+func on_character_selected(_character_id: String):
+	refresh_character_cards()
 
 
 func on_back_button_pressed():
